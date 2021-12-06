b4227852c5c255e2c309152a7bff916be68af3ea
Hiske Overweg <hiske.overweg@wur.nl>
2021-12-06 08:05:00 +0100

    day 5, part2

--- 2021/dec5.jl
+++ 2021/dec5.jl
@@ -9,7 +9,6 @@ coordinates = map(parsevec, collect(the_array))
 
 max_size = max(vcat(hcat(coordinates...)...)...) + 1
 grid = zeros((max_size, max_size))
-
 for index in 1:rows
     (x1, y1, x2, y2) = vcat(coordinates[index, :]...) + ones(Int, 4, 1)
     if x1 == x2
@@ -20,6 +19,36 @@ for index in 1:rows
         maxx = max(x1, x2)
         minx = min(x1, x2)
         grid[minx:maxx, y1] .+=1
+    end
+end
+
+println(sum(map(x -> x > 1, grid)))
+
+grid = zeros((max_size, max_size))
+for index in 1:rows
+    (x1, y1, x2, y2) = vcat(coordinates[index, :]...) + ones(Int, 4, 1)
+    if x1 == x2
+        maxy = max(y1, y2)
+        miny = min(y1, y2)
+        grid[x1, miny:maxy] .+=1
+    elseif y1 == y2
+        maxx = max(x1, x2)
+        minx = min(x1, x2)
+        grid[minx:maxx, y1] .+=1
+    else
+        if x1 < x2
+            x_sequence = collect(x1:x2)
+        else
+            x_sequence = reverse(collect(x2:x1))
+        end
+        if y1 < y2
+            y_sequence = collect(y1:y2)
+        else
+            y_sequence = reverse(collect(y2:y1))
+        end
+        for (x,y) in zip(x_sequence, y_sequence)
+            grid[x, y] +=1
+        end
     end
 end
 
