af22c419511856486a9e637ba2e2bf00a225ff71
Hiske Overweg <hiske.overweg@wur.nl>
2021-12-07 17:50:14 +0100

    day 7, part 1

--- 2021/dec7.jl
+++ 2021/dec7.jl
@@ -2,11 +2,4 @@ using DelimitedFiles
 using Statistics
 
 the_array = vec(readdlm("input7.txt", ',', Int, '\n'))
-
-starting_point = median(the_array)
-
-function calculate_fuel(point, crabs)
-    return mapreduce(x -> abs(x-point), +, crabs)
-end
-
-println(Int(calculate_fuel(starting_point, the_array)))
+println(mapreduce(x -> abs(x-median(the_array)), +, the_array))
