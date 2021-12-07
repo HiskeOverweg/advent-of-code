2e0a44d07c1ffb04ce494d276d924344d5938b73
Hiske Overweg <hiske.overweg@wur.nl>
2021-12-07 18:18:27 +0100

    added 2nd part day 7

--- 2021/dec7.jl
+++ 2021/dec7.jl
@@ -2,4 +2,19 @@ using DelimitedFiles
 using Statistics
 
 the_array = vec(readdlm("input7.txt", ',', Int, '\n'))
-println(mapreduce(x -> abs(x-median(the_array)), +, the_array))
+println(Int(mapreduce(x -> abs(x-median(the_array)), +, the_array)))
+
+function fuel_per_crab(distance)
+    return Int(0.5 * (distance + 1) * distance)
+end
+
+function total_fuel(position, array)
+    return mapreduce(x-> fuel_per_crab(abs(x-position)),+, collect(array))
+end
+
+max_pos = maximum(the_array)
+min_pos = minimum(the_array)
+fuels = map(x -> total_fuel(x, the_array), collect(min_pos:max_pos))
+println(minimum(fuels))
+
+
